aba761046c5dbfd1ee784e9eaa282c92c350ea09
srehan1 <syed.rehan@shoppertrak.com>
2023-04-26 13:06:03 +0530

    Displaying all campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import web3 from "../ethereum/web3";
-import instance from "../ethereum/factory";
-import factory from "../ethereum/factory";
 import "semantic-ui-css/semantic.min.css";
+import { Card } from "semantic-ui-react";
+// import web3 from "../ethereum/web3";
+// import instance from "../ethereum/factory";
+import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
   // To get initial data using nextjs server from Ethereum
@@ -11,8 +12,22 @@ class CampaignIndex extends Component {
     return { campaignList };
   }
 
+  renderCampaigns() {
+    // Read list of campaigns and convert them into a group of cards using map()
+    // `fluid: true` property helps in covering full width of browser for a card
+    const items = this.props.campaignList.map((address) => {
+      return {
+        header: address,
+        description: <a>View campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
-    return <div>{this.props.campaignList}</div>;
+    return <div data-testid="campaign-list">{this.renderCampaigns()}</div>;
   }
 }
 
