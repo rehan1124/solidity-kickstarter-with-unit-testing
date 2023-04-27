461d600d32da88d5e0293197506e8a1db76d35b0
srehan1 <syed.rehan@shoppertrak.com>
2023-04-27 20:10:00 +0530

    Added new page for campaign creation

---
+++ pages/campaigns/new.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import Layout from "../../components/Layout";
+
+class CampaignNew extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>New campaign.</h1>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignNew;
