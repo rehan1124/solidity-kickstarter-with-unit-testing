dad9578277e6924a5941b58be86e1fd4de8d7928
srehan1 <syed.rehan@shoppertrak.com>
2023-05-12 17:23:46 +0530

    Updated approving the request || Added link to go back previous page

--- components/RequestListTable.js
+++ components/RequestListTable.js
@@ -1,9 +1,34 @@
-import React from "react";
-import { Table } from "semantic-ui-react";
+// Standard library import
+import React, { useState } from "react";
+
+// Any 3rd party imports
+import { Table, Button } from "semantic-ui-react";
+
+// Local components
+import Error from "./Error";
+
+// Ethereum imports
 import web3 from "../ethereum/web3";
 import campaign from "../ethereum/campaign";
 
-const RequestListTable = ({ requestsList, totalApprovers }) => {
+const RequestListTable = ({ address, requestsList, totalApprovers }) => {
+  // Setting state
+  const [errorMessage, setErrorMessage] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  const handleApproveClick = async (event) => {
+    setHasError(false);
+    try {
+      const cn = await campaign(address);
+      const accounts = await web3.eth.getAccounts();
+      const requestIndex = Number(event.target.getAttribute("data-button-id"));
+      await cn.methods.approveRequest(requestIndex).send({ from: accounts[0] });
+      window.location.reload(false);
+    } catch (err) {
+      setHasError(true);
+      setErrorMessage(err.message);
+    }
+  };
   const addRow = () => {
     return requestsList.map((request, index) => {
       return (
@@ -17,27 +42,40 @@ const RequestListTable = ({ requestsList, totalApprovers }) => {
           <Table.Cell>
             {request.approvalCount} / {totalApprovers}
           </Table.Cell>
-          <Table.Cell>Approve</Table.Cell>
-          <Table.Cell>Finalize</Table.Cell>
+          <Table.Cell>
+            <Button
+              color="green"
+              onClick={handleApproveClick}
+              data-button-id={index}
+            >
+              Approve
+            </Button>
+          </Table.Cell>
+          <Table.Cell>
+            <Button>Finalize</Button>
+          </Table.Cell>
         </Table.Row>
       );
     });
   };
   return (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>ID</Table.HeaderCell>
-          <Table.HeaderCell>Description</Table.HeaderCell>
-          <Table.HeaderCell>Amount</Table.HeaderCell>
-          <Table.HeaderCell>Recipient</Table.HeaderCell>
-          <Table.HeaderCell>Approval count</Table.HeaderCell>
-          <Table.HeaderCell>Approve</Table.HeaderCell>
-          <Table.HeaderCell>Finalize</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>{addRow()}</Table.Body>
-    </Table>
+    <>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+            <Table.HeaderCell>Amount</Table.HeaderCell>
+            <Table.HeaderCell>Recipient</Table.HeaderCell>
+            <Table.HeaderCell>Approval count</Table.HeaderCell>
+            <Table.HeaderCell>Approve</Table.HeaderCell>
+            <Table.HeaderCell>Finalize</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{addRow()}</Table.Body>
+      </Table>
+      {hasError && <Error errorMessage={errorMessage} />}
+    </>
   );
 };
 
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,16 @@
+// Standard library import
 import React from "react";
+
+// Any 3rd party imports
 import { Header, Button, Table } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+
+// Local components import
 import Layout from "../../../components/Layout";
 import RequestListTable from "../../../components/RequestListTable";
 import { Link } from "../../../routes";
+
+// Ethereum imports
 import campaign from "../../../ethereum/campaign";
 
 const RequestIndex = ({ address, requestsList, totalApprovers }) => {
@@ -12,6 +19,9 @@ const RequestIndex = ({ address, requestsList, totalApprovers }) => {
       <Header size="medium">Requests list</Header>
       <Link route={`/campaigns/${address}/requests/new`}>
         <Button primary>Add request</Button>
+      </Link>
+      <Link route={`/campaigns/${address}`}>
+        <u style={{ float: "right" }}>Back</u>
       </Link>
       <RequestListTable
         address={address}
@@ -35,6 +45,9 @@ RequestIndex.getInitialProps = async (props) => {
       })
   );
   console.log(`Requests under the campaign ${address}`, requestsList);
+  console.log(
+    `Total approvers or contributors to the campaign: ${totalApprovers}`
+  );
   return { address, requestsCount, requestsList, totalApprovers };
 };
 
